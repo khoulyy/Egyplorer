9bcad6f4425714d25cc4bd6db36673fbcfd635fa
khoulyy <abdoelkhouly555@gmail.com>
2023-05-14 02:54:32 +0300

    fixed button color in your response page

--- lib/screens/signup_step3_screen.dart
+++ lib/screens/signup_step3_screen.dart
@@ -129,7 +129,7 @@ class _SignUpStep3ScreenState extends State<SignUpStep3Screen> {
                 onPressed: () {
                   Navigator.pushNamed(context, HomeScreen.id);
                 },
-                backgroundColor: kSecoundaryColor,
+                fontColor: kMainColor,
                 fontSize: 22,
                 width: 141,
                 height: 51,
